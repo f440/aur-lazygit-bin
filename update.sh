ce5a0528cef4c9d0ce591a97360dcce396249711
f440 <freq440@gmail.com>
2021-02-12 20:50:08 +0900

    Support arm architecture

--- update.sh
+++ update.sh
@@ -6,12 +6,21 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 echo "version=$version"
+sed -i.bak -e "s/^pkgver=.*/pkgver=$version/" PKGBUILD
+
+declare -A archs=(
+    # file suffix -> $CARCH
+    ["x86_64"]="x86_64"
+    ["armv6"]="armv6h"
+    ["arm64"]="aarch64"
+)
 
 curl -sLO https://github.com/jesseduffield/lazygit/releases/download/v$version/checksums.txt
-shasum=$(grep Linux_x86_64 checksums.txt | awk '{print $1}')
-echo shasum=$shasum
+for arch in "${!archs[@]}"; do
+    shasum=$(grep Linux_$arch checksums.txt | awk '{print $1}')
+    sed -i.bak -e "s/^sha256sums_${archs[$arch]}=.*/sha256sums_${archs[$arch]}=('$shasum')/" PKGBUILD
+done
 rm -f checksums.txt
 
-sed -i.bak -e "s/^pkgver=.*/pkgver=$version/" -e "s/^sha256sums=.*/sha256sums=('$shasum')/" PKGBUILD
 echo update PKGBUILD
 rm -f PKGBUILD.bak
